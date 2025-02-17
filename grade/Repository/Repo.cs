0a5b8c6aed86f1b7bd24d6518c47f25c6de5edb6
batoul tabbal <tota-02-syr@hotmail.com>
2025-02-17 14:00:02 +0300

    adding repository

---
+++ grade/Repository/Repo.cs
@@ -0,0 +1,29 @@
+﻿namespace grade.Repository
+{
+    public class Repo
+    {
+        public int Id { get; set; }
+        public double Area { get; set; }
+        public double High {  get; set; }
+        public string Address { get; set; }
+        public List<int> NumOfSections { get; set; }
+        public Repo ( int id, double area, double high, string address, int sections)
+        { 
+            Id = id;
+            Area = area;
+            High = high;
+            Address = address;
+            NumOfSections = new List<int>();
+        }
+        public void AddSection(int sectionId)
+        {
+            if (!NumOfSections.Contains(sectionId))
+            { NumOfSections.Add(sectionId); }
+        }
+        public void RemoveSection(int sectionId)
+        {
+            if (NumOfSections.Contains(sectionId))
+            { NumOfSections.Remove(sectionId); }
+        }
+    }
+}
