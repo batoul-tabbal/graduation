9173c3461bf2cde35ce8466db217a7a5cde7e370
batoul tabbal <tota-02-syr@hotmail.com>
2025-02-17 14:28:13 +0300

    adding section

---
+++ grade/Repository/Section.cs
@@ -0,0 +1,33 @@
+﻿namespace grade.Repository
+{
+    public class Section
+    {
+        public int SectionId { get; set; }
+        public double SectionArea { get; set; }
+        public double SectionHigh { get; set; }
+        public int NumOfItems { get; set; }
+
+        public Section(int sectionId, double area, double high, int items)
+        { 
+            SectionId = sectionId;
+            SectionArea = area;
+            SectionHigh = high;
+            NumOfItems = 0;
+        }
+        public void AddItems( int count)
+        {
+            NumOfItems += count;
+        }
+        public void RemoveItems(int count)
+        {
+            if (count <= NumOfItems)
+            {
+                NumOfItems -= count;
+            }
+            else
+            {
+                throw new InvalidOperationException(" Cannot remove more items than available");
+            }
+        }
+    }
+}
